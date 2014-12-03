2f097518dd6c570750a662aeca577751a89f53c9
Ryan Schmukler <rschmukler@gmail.com>
2014-12-02 19:46:52 -0600

    add notification service

---
+++ services/NotificationService.swift
@@ -0,0 +1,24 @@
+import Foundation
+import AppKit
+
+class NotificationService {
+  class var notificationCenter: NSUserNotificationCenter {
+    return NSUserNotificationCenter.defaultUserNotificationCenter()
+  }
+  
+  class func simpleNotification(title: String, description: String) {
+    var notification = NSUserNotification()
+    notification.title = title
+    notification.informativeText = description
+    
+    notificationCenter.deliverNotification(notification)
+  }
+  
+  class func simpleNotification(title: String, description: String, image: NSImage) {
+    var notification = NSUserNotification()
+    notification.title = title
+    notification.informativeText = description
+    notification.contentImage = image
+    notificationCenter.deliverNotification(notification)
+  }
+}
