d22869ef40ad28d86bdf907da6429c98feb74961
Ryan Schmukler <rschmukler@gmail.com>
2014-12-05 22:23:29 -0500

    improve image api

--- services/NotificationService.swift
+++ services/NotificationService.swift
@@ -14,7 +14,7 @@ class NotificationService {
     notificationCenter.deliverNotification(notification)
   }
   
-  class func simpleNotification(title: String, description: String, image: NSImage) {
+  class func simpleNotification(title: String, description: String, withImage image: NSImage) {
     var notification = NSUserNotification()
     notification.title = title
     notification.informativeText = description
